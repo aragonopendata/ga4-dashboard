ed66bbe082654b16d1d53f19f9f845c5e13d9182
beni-smartup <bmorales@smartup.es>
2023-05-11 13:47:06 +0200

    Aplying styles

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -12,18 +12,19 @@ interface Dates {
 })
 
 export class DropdownComponent {
-  selectedValue: string = '';
+  baseUrl: string = 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A';
+  endUrl: string = ')&hide-filter-bar=true%22';
 
   dates: Dates[] = [
-    { value: 'Año actual', iframeUrl: 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Fd%2Cto%3Anow))&hide-filter-bar=true' },
+    { value: 'Año actual', iframeUrl: `${this.baseUrl}(from%3Anow-1y%2Fd%2Cto%3Anow)${this.endUrl}` },
+    { value: '30 días', iframeUrl: `${this.baseUrl}(from%3Anow-30d%2Fd%2Cto%3Anow)${this.endUrl}` },
 
-    { value: '30 días', iframeUrl: 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-30d%2Fd%2Cto%3Anow))&hide-filter-bar=true' },
+    { value: '15 días', iframeUrl: `${this.baseUrl}(from%3Anow-15d%2Fd%2Cto%3Anow)${this.endUrl}` },
 
-    { value: '15 días', iframeUrl: 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15d%2Fd%2Cto%3Anow))&hide-filter-bar=true%22' },
+    { value: '7 días', iframeUrl: `${this.baseUrl}(from%3Anow-7d%2Fd%2Cto%3Anow)${this.endUrl}` },
 
-    { value: '7 días', iframeUrl: 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-7d%2Fd%2Cto%3Anow))&hide-filter-bar=true%22' },
-
-    { value: 'Ayer', iframeUrl: 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1d%2Fd%2Cto%3Anow))&hide-filter-bar=true%22' }
+    { value: 'Ayer', iframeUrl: `${this.baseUrl}(from%3Anow-1d%2Fd%2Cto%3Anow)${this.endUrl}` }
   ];
 
+  selectedValue: string = this.dates[1].iframeUrl;
 }
