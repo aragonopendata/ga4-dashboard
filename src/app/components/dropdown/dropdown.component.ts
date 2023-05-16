3542a2a28089fd2246aec18e724ceaf24b0717d3
pmontesinosSmartup <pmontesinos@smartup.es>
2023-05-16 12:57:41 +0200

    Cambios aplicados en las cajas de selección, necesario repaso del ngmodel para aplicar cambios y que actualicen selectedValue.iframe

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
-interface Dates {
+interface URLBuilder {
   value: string;
   iframeUrl: string;
+  urlSnippet: string;
 }
 
 @Component({
@@ -11,20 +13,40 @@ interface Dates {
   styleUrls: ['./dropdown.component.scss']
 })
 
+
 export class DropdownComponent {
-  baseUrl: string = 'https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/ad4977e0-cf06-11ed-91b6-b3f4561f6def?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A';
-  endUrl: string = ')&hide-filter-bar=true%22';
 
-  dates: Dates[] = [
-    { value: 'Año actual', iframeUrl: `${this.baseUrl}(from%3Anow-1y%2Fd%2Cto%3Anow)${this.endUrl}` },
-    { value: '30 días', iframeUrl: `${this.baseUrl}(from%3Anow-30d%2Fd%2Cto%3Anow)${this.endUrl}` },
+  urlHead: string = environment.baseUrl
+  urlBody: string = ",refreshInterval:(pause:!t,value:0),";
+  urlTail: string = ")&hide-filter-bar=true";
 
-    { value: '15 días', iframeUrl: `${this.baseUrl}(from%3Anow-15d%2Fd%2Cto%3Anow)${this.endUrl}` },
+  selectedPortal: string = '(query:presupuesto.aragon.es),type:phrase),query:(match_phrase:(portal:presupuesto.aragon.es))))';
+  selectedDate: string = 'time:(from%3Anow-30d%2Fd%2Cto%3Anow))';
 
-    { value: '7 días', iframeUrl: `${this.baseUrl}(from%3Anow-7d%2Fd%2Cto%3Anow)${this.endUrl}` },
+  changePortal(newPortal: any) {
+    console.log(newPortal)
+    this.selectedPortal = newPortal;
+  }
+  changeDate(newDate: string) {
+    this.selectedDate = newDate;
+  }
 
-    { value: 'Ayer', iframeUrl: `${this.baseUrl}(from%3Anow-1d%2Fd%2Cto%3Anow)${this.endUrl}` }
+  dates: URLBuilder[] = [
+    { value: 'Año actual', iframeUrl: `${this.urlHead}${this.selectedPortal}${this.urlBody}time:(from:now-1y%2Fd,to:now)${this.urlTail}`, urlSnippet: 'time:(from:now-1y%2Fd,to:now)' },
+
+    { value: '30 días', iframeUrl: `${this.urlHead}${this.selectedPortal}${this.urlBody}time:(from:now-30d%2Fd,to:now)${this.urlTail}`, urlSnippet: 'time:(from:now-30d%2Fd,to:now)' },
+
+    { value: '15 días', iframeUrl: `${this.urlHead}${this.selectedPortal}${this.urlBody}time:(from:now-15d%2Fd,to:now)${this.urlTail}`, urlSnippet: 'time:(from:now-15d%2Fd,to:now)' },
+
+    { value: '7 días', iframeUrl: `${this.urlHead}${this.selectedPortal}${this.urlBody}time:(from:now-7d%2Fd,to:now)${this.urlTail}`, urlSnippet: 'time:(from:now-7d%2Fd,to:now)' },
+
+    { value: 'Ayer', iframeUrl: `${this.urlHead}${this.selectedPortal}${this.urlBody}time:(from:now-1d%2Fd,to:now)${this.urlTail}`, urlSnippet: 'time:(from:now-1d%2Fd,to:now)' }
   ];
 
-  selectedValue: string = this.dates[1].iframeUrl;
+  portals: URLBuilder[] = [
+    { value: 'presupuestos.aragon.es', iframeUrl: `${this.urlHead}(query:presupuesto.aragon.es),type:phrase),query:(match_phrase:(portal:presupuesto.aragon.es))))${this.urlBody}${this.selectedDate}${this.urlTail}`, urlSnippet: '(query:presupuesto.aragon.es),type:phrase),query:(match_phrase:(portal:presupuesto.aragon.es))))' },
+  ];
+
+
+  selectedValue: URLBuilder = { value: "", iframeUrl: this.dates[1].iframeUrl, urlSnippet: "" };
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+//Despliegue: ng build --configuration=development
+
+export const environment = {
+    production: false,
+    baseUrl: "https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'6dd1cc00-d39f-11ed-91b6-b3f4561f6def',key:portal,negate:!f,params:"
+};
