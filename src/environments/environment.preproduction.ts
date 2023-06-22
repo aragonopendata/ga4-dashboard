2698893da19ec6b56eb1aaf41705facbfd85d20f
pmontesinosSmartup <107853611+pmontesinosSmartup@users.noreply.github.com>
2023-06-22 13:43:52 +0200

    Update environment.preproduction.ts

--- src/environments/environment.preproduction.ts
+++ src/environments/environment.preproduction.ts
@@ -3,6 +3,6 @@
 export const environment = {
     production: false,
     environment_host : "https://prependata.aragon.es",
-    baseUrl: "https://preopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'6dd1cc00-d39f-11ed-91b6-b3f4561f6def',key:portal,negate:!f,params:",
+    baseUrl:"https://preopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(",
     AODBACK_LOGSTASH_ENDPOINT:'https://preopendata.aragon.es/aod/services/web/analytics/files'
 };
