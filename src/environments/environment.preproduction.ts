7cfd86aad60c7f5c522000922e3a835118f4c03a
koldo-zabaleta <75609748+koldo-zabaleta@users.noreply.github.com>
2023-06-28 12:17:37 +0200

    Update environment.preproduction.ts

--- src/environments/environment.preproduction.ts
+++ src/environments/environment.preproduction.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: false,
-    environment_host : "https://prependata.aragon.es",
+    environment_host : "https://preopendata.aragon.es",
     baseUrl:"https://preopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(",
     AODBACK_LOGSTASH_ENDPOINT:'https://preopendata.aragon.es/aod/services/web/analytics/files'
 };
