714c045126537985accea6316083041f6fedeae7
pmontesinosSmartup <107853611+pmontesinosSmartup@users.noreply.github.com>
2023-07-03 10:43:20 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 export const environment = {
     production: true,
     environment_host : "https://opendata.aragon.es",
-    baseUrl: "https://desopendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(",
+    baseUrl: "https://opendataei2a.aragon.es/cobertura/kibana/app/dashboards?auth_provider_hint=anonymous1#/view/a0c4c7f0-f2fa-11ed-8596-6dee554197cb?embed=true&_g=(filters:!(",
     AODBACK_LOGSTASH_ENDPOINT:'https://opendata.aragon.es/aod/services/web/analytics/files'
 
 };
