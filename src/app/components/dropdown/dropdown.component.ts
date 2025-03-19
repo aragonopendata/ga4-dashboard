5e5d5e44645667e957581e93916667a5ab3659f4
koldo-zabaleta <75609748+koldo-zabaleta@users.noreply.github.com>
2025-03-19 14:20:07 +0100

    Portal types changed to analytics

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -39,7 +39,7 @@ export class DropdownComponent implements OnInit {
 
 
     data.forEach((element: any) => {
-      if (element.status == 1 && element.type == 'analytics_GA4') {
+      if (element.status == 1 && element.type == 'analytics') {
         this.portals.push({ value: element.portal_name, iframeUrl: `${this.urlHead}('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'6dd1cc00-d39f-11ed-91b6-b3f4561f6def',key:portal,negate:!f,params:(view:${element.view}),type:phrase),query:(match_phrase:(view:${element.view})))${this.urlBody}${this.selectedDate}${this.urlTail}`, urlSnippet: `('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'6dd1cc00-d39f-11ed-91b6-b3f4561f6def',key:portal,negate:!f,params:(query:${element.url}),type:phrase),query:(match_phrase:(portal:${element.url})))` });
         this.all_portals_id.push(element.id_logstash)
       }
